ab4fa708b060e12e73cf75ca4b49b1786269f4c9
George Smyrnaios <smyrgeorge@gmail.com>
2023-09-16 19:59:30 +0200

    Using ProtobufConverter.

--- kafka-connect-json-to-proto/src/main/kotlin/io/smyrgeorge/test/Main.kt
+++ kafka-connect-json-to-proto/src/main/kotlin/io/smyrgeorge/test/Main.kt
@@ -1,9 +1,25 @@
 package io.smyrgeorge.test
 
+import io.smyrgeorge.test.domain.Customer
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.protobuf.ProtoBuf
+import kotlinx.serialization.protobuf.schema.ProtoBufSchemaGenerator
+
 class Main
 
-@Suppress("UNUSED_PARAMETER")
-fun main(args: Array<String>) {
+@OptIn(ExperimentalSerializationApi::class)
+fun main() {
     val c = ProtobufConverter()
     println(c)
+
+    val customer = Customer(5, "test", "test", "test")
+    val bytes = ProtoBuf.encodeToByteArray(customer)
+    val customer1 = ProtoBuf.decodeFromByteArray<Customer>(bytes)
+
+    println("$customer :: $customer1")
+
+    val schema = ProtoBufSchemaGenerator.generateSchemaText(Customer.serializer().descriptor)
+    println(schema)
 }
