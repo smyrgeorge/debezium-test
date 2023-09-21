f44fd76d4e8946bed9cd5de07b70171678f7fb83
George Smyrnaios <smyrgeorge@gmail.com>
2023-09-21 20:17:58 +0200

    Optimized ProtobufConverter.

--- kafka-connect-json-to-proto/src/main/kotlin/io/smyrgeorge/test/Main.kt
+++ kafka-connect-json-to-proto/src/main/kotlin/io/smyrgeorge/test/Main.kt
@@ -4,6 +4,7 @@ import com.google.protobuf.DynamicMessage
 import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient
 import io.confluent.kafka.schemaregistry.client.SchemaMetadata
 import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema
+import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchemaProvider
 
 class Main
 
@@ -12,7 +13,13 @@ fun main() {
     println(c)
 
     val schemaRegistryUrl = "http://localhost:58002/apis/ccompat/v7"
-    val schemaRegistryClient = CachedSchemaRegistryClient(schemaRegistryUrl, 100)
+    val schemaProvider = ProtobufSchemaProvider()
+    val schemaRegistryClient = CachedSchemaRegistryClient(
+        /* baseUrls = */ schemaRegistryUrl,
+        /* identityMapCapacity = */ 100,
+        /* providers = */ listOf(schemaProvider),
+        /* originals = */ emptyMap<String, Any>()
+    )
 
     val topic = "dbserver1.inventory.customers"
     val meta: SchemaMetadata = schemaRegistryClient.getLatestSchemaMetadata(topic)
--- kafka-connect-json-to-proto/src/main/kotlin/io/smyrgeorge/test/ProtobufConverter.kt
+++ kafka-connect-json-to-proto/src/main/kotlin/io/smyrgeorge/test/ProtobufConverter.kt
@@ -1,10 +1,12 @@
 package io.smyrgeorge.test
 
+import com.google.protobuf.Descriptors
 import com.google.protobuf.DynamicMessage
 import com.google.protobuf.util.JsonFormat
 import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient
 import io.confluent.kafka.schemaregistry.client.SchemaMetadata
 import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema
+import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchemaProvider
 import org.apache.kafka.connect.data.Schema
 import org.apache.kafka.connect.data.SchemaAndValue
 import org.apache.kafka.connect.storage.Converter
@@ -15,10 +17,16 @@ class ProtobufConverter : Converter {
     private val jsonNodeConverter: JsonNodeConverter = JsonNodeConverter()
 
     private val schemaRegistryUrl = "http://apicurio:8080/apis/ccompat/v7"
-    private val schemaRegistryClient = CachedSchemaRegistryClient(schemaRegistryUrl, 100)
+    private val schemaProvider = ProtobufSchemaProvider()
+    private val schemaRegistryClient = CachedSchemaRegistryClient(
+        /* baseUrls = */ schemaRegistryUrl,
+        /* identityMapCapacity = */ 100,
+        /* providers = */ listOf(schemaProvider),
+        /* originals = */ emptyMap<String, Any>()
+    )
 
-    // <topic, dynamic message builder>
-    private val protoCache = ConcurrentHashMap<String, DynamicMessage.Builder>()
+    // <topic, message protobuf descriptor>
+    private val protoCache = ConcurrentHashMap<String, Descriptors.Descriptor>()
 
     override fun configure(configs: MutableMap<String, *>, isKey: Boolean) {
         println("[ProtobufConverter] :: Hello!")
@@ -27,28 +35,28 @@ class ProtobufConverter : Converter {
 
     override fun fromConnectData(topic: String, schema: Schema, value: Any): ByteArray {
         val json: String = jsonNodeConverter.fromConnectDataToJson(topic, schema, value)
-        val builder: DynamicMessage.Builder = protoDescriptorOf(topic)
+        val descriptor: Descriptors.Descriptor = protoDescriptorOf(topic)
+        val builder = DynamicMessage.newBuilder(descriptor)
         JsonFormat.parser().ignoringUnknownFields().merge(json, builder)
         return builder.build().toByteArray()
     }
 
-    private fun protoDescriptorOf(topic: String): DynamicMessage.Builder {
-        val cached: DynamicMessage.Builder? = protoCache[topic]
+    private fun protoDescriptorOf(topic: String): Descriptors.Descriptor {
+        val cached: Descriptors.Descriptor? = protoCache[topic]
         return if (cached != null) {
             cached
         } else {
             val meta: SchemaMetadata = schemaRegistryClient.getLatestSchemaMetadata(topic)
 
             val protoSchema = try {
-                schemaRegistryClient.getSchemaBySubjectAndId(topic, meta.id) as ProtobufSchema
+                schemaRegistryClient.getSchemaById(meta.id) as ProtobufSchema
             } catch (e: Exception) {
-                println(e.message)
                 throw e
             }
 
-            val builder = DynamicMessage.newBuilder(protoSchema.toDescriptor())
-            protoCache[topic] = builder
-            builder
+            val descriptor = protoSchema.toDescriptor()
+            protoCache[topic] = descriptor
+            descriptor
         }
     }
 
